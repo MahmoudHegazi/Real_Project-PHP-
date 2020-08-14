c0a8ea365a3c2373f8d2166f07a535ad7ca99774
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-08-15 01:55:19 +0200

    Update myadmin.sql

--- myadmin.sql
+++ myadmin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 14, 2020 at 10:16 PM
+-- Generation Time: Aug 14, 2020 at 11:53 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.19
 
@@ -95,6 +95,52 @@ CREATE TABLE `maintenance` (
 INSERT INTO `maintenance` (`id`, `device_id`, `supplier_id`, `creator_id`, `price`, `last_employee_id`) VALUES
 (2, 6, 1, 3, '600', 6),
 (3, 7, 2, 3, '300', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(20) NOT NULL,
+  `subject` varchar(30) NOT NULL,
+  `body` text NOT NULL,
+  `image` text NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `reciver_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `subject`, `body`, `image`, `sender_id`, `reciver_id`) VALUES
+(1, 'Hello World', 'Welcome this my First Message For you Hello', '', 1, 3),
+(2, 'Second Message', 'Hello My friend are you ready for meeting', '/images/laptop.jpg', 2, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(100) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `reciver_id` int(11) NOT NULL,
+  `icon` varchar(600) NOT NULL,
+  `title` enum('new_message','create_ticket','delete_ticket','added_device','closed_ticket','comment_add','user_created','user_removed','new_maintenance','image_updated','blocked_messages') NOT NULL,
+  `header` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `content`, `reciver_id`, `icon`, `title`, `header`) VALUES
+(1, 'You Have Got, New Message from Mahmoud Hegazy', 3, '/images/rsz_new_message.jpg', 'new_message', 'New Message'),
+(2, 'You Have Got, New Message from Mahmoud Hegazy', 10, '/images/rsz_new_message.jpg', 'new_message', 'New Message');
 
 -- --------------------------------------------------------
 
@@ -233,6 +279,21 @@ ALTER TABLE `maintenance`
   ADD KEY `maintenance_ibfk_1` (`creator_id`);
 
 --
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sender_id` (`sender_id`),
+  ADD KEY `reciver_id` (`reciver_id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reciver_id` (`reciver_id`);
+
+--
 -- Indexes for table `suppliers`
 --
 ALTER TABLE `suppliers`
@@ -284,6 +345,18 @@ ALTER TABLE `maintenance`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
@@ -332,6 +405,19 @@ ALTER TABLE `maintenance`
   ADD CONSTRAINT `maintenance_ibfk_2` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`sup_id`),
   ADD CONSTRAINT `maintenance_ibfk_4` FOREIGN KEY (`device_id`) REFERENCES `workstation` (`id`),
   ADD CONSTRAINT `maintenance_ibfk_5` FOREIGN KEY (`last_employee_id`) REFERENCES `employees` (`id`);
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`myid`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`reciver_id`) REFERENCES `users` (`myid`);
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`reciver_id`) REFERENCES `users` (`myid`);
 
 --
 -- Constraints for table `ticket`
